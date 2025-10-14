0f1a1dd29e2645a9d8f37bae39caa2f44dc4d0b8
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-14 14:42:36 +0200

    create navbar

---
+++ src/app/components/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {}
+}
