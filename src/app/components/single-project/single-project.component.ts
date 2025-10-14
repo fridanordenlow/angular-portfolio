371071acff9dea9eecd4ae99a7dccf93a03aed36
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-14 14:40:31 +0200

    update single project

--- src/app/components/single-project/single-project.component.ts
+++ src/app/components/single-project/single-project.component.ts
@@ -1,13 +1,14 @@
 import { Component, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { IProject } from '../../models/IProject';
 
 @Component({
   selector: 'app-single-project',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './single-project.component.html',
   styleUrl: './single-project.component.scss',
 })
 export class SingleProjectComponent {
-  project = input<IProject>();
+  project = input.required<IProject>();
 }
