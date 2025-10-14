b67758620129d9aaaf9c552efbff7ae52264ca52
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-14 16:06:12 +0200

    create contact

---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,11 @@
+import { Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss',
+})
+export class ContactComponent implements OnInit {
+  ngOnInit(): void {}
+}
