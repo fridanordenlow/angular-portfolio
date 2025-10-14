87d5feb37192114a145f4330d870fa9da1b5264f
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-14 16:05:32 +0200

    setup routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { ProjectsComponent } from './components/projects/projects.component';
+// import { ProjectsComponent } from './components/pages/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ProjectsComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NotFoundComponent },
+];
