8125cf2b2216e1f207e0ada5b1820343b638b051
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-14 14:40:10 +0200

    update header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
