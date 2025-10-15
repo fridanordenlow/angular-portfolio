36abc772c3bf461811c8db48e0f928b0e806ae91
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-15 17:18:33 +0200

    add a cv page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CvComponent } from './pages/cv/cv.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'cv', component: CvComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', component: NotFoundComponent },
 ];
---
+++ src/app/pages/cv/cv.component.ts
@@ -0,0 +1,11 @@
+import { Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cv',
+  imports: [],
+  templateUrl: './cv.component.html',
+  styleUrl: './cv.component.scss',
+})
+export class CvComponent implements OnInit {
+  ngOnInit(): void {}
+}
