4ed5e6c5c17b3ca5231c579ff33001f8b97232a0
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-15 18:12:23 +0200

    add and style content of home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, type OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
