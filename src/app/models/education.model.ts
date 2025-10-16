4da39b736d05a19e12d28ba5902c4a545a264db6
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-16 13:36:56 +0200

    create interfaces for education and jobs

---
+++ src/app/models/education.model.ts
@@ -0,0 +1,12 @@
+export interface IEducation {
+  id: string;
+  school: string;
+  program: string;
+  degree?: string;
+  credits?: string;
+  fieldOfStudy: string;
+  startDate: string;
+  endDate: string;
+  location: string;
+  description: string;
+}
---
+++ src/app/models/job.model.ts
@@ -0,0 +1,9 @@
+export interface IJob {
+  id: string;
+  company: string;
+  position: string;
+  startDate: string;
+  endDate: string;
+  location: string;
+  description: string;
+}
