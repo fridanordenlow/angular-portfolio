6ad65edc3d066a52a4887f05c973ea5bce31a070
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-16 13:39:52 +0200

    update cv component with education and job signals

--- src/app/pages/cv/cv.component.ts
+++ src/app/pages/cv/cv.component.ts
@@ -1,11 +1,33 @@
-import { Component, type OnInit } from '@angular/core';
+import { Component, signal, WritableSignal, type OnInit } from '@angular/core';
+import { IEducation } from '../../models/education.model';
+import { EducationService } from '../../services/education.service';
+import { EducationCardComponent } from '../../components/education-card/education-card.component';
+import { JobCardComponent } from '../../components/job-card/job-card.component';
+import { IJob } from '../../models/job.model';
+import { JobService } from '../../services/job.service';
 
 @Component({
   selector: 'app-cv',
-  imports: [],
+  imports: [EducationCardComponent, JobCardComponent],
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.scss',
 })
 export class CvComponent implements OnInit {
-  ngOnInit(): void {}
+  educationSignal: WritableSignal<IEducation[]> = signal([]);
+  jobSignal: WritableSignal<IJob[]> = signal([]);
+
+  constructor(
+    private educationService: EducationService,
+    private jobService: JobService
+  ) {}
+
+  ngOnInit(): void {
+    this.educationService.getEducation().subscribe((education) => {
+      this.educationSignal.set(education);
+    });
+
+    this.jobService.getJobs().subscribe((jobs) => {
+      this.jobSignal.set(jobs);
+    });
+  }
 }
