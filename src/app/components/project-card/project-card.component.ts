4aaccfbac5f6d035a0ce8de846c9b93fed1d4f55
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-16 17:05:26 +0200

    update project card

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,12 +1,12 @@
 import { Component, input } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { IProject } from '../../models/project.model';
 import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [NgOptimizedImage, ButtonComponent],
+  imports: [CommonModule, NgOptimizedImage, ButtonComponent],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss',
 })
