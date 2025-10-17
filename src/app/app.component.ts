a6340b7038e509f055505353ba57a0f303e785db
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-17 13:22:24 +0200

    create and implement breakpoint service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { BreakpointService } from './services/breakpoint.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-portfolio';
+
+  constructor(public breakpointService: BreakpointService) {}
 }
---
+++ src/app/services/breakpoint.service.ts
@@ -0,0 +1,42 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { debounceTime, fromEvent, startWith } from 'rxjs';
+
+// Like global context/ store
+@Injectable({
+  providedIn: 'root',
+})
+export class BreakpointService {
+  // Signal = Angular's reactive state system HOWEVER it is reactive everywhere
+  // automatically without one needing to trigger an update manually
+  // const [width, setWidth] = useState(window.innerWidth);
+
+  // Signal that holds current device width
+  width = signal(window.innerWidth);
+  // Add signal for height possibly
+
+  // My breakpoints
+  private readonly mobileMax = 667;
+  private readonly tabletMax = 850;
+
+  // Computed signals (reactive)
+  // They will auto update if a width signal changes
+  // Collect the value of the signal width by calling it through this.width()
+  // Compare the width to mobileMax
+  // The comparison returns either true or false
+  isMobile = computed(() => this.width() <= this.mobileMax);
+  isTablet = computed(
+    () => this.width() > this.mobileMax && window.innerWidth <= this.tabletMax
+  );
+  isDesktop = computed(() => this.width() > this.tabletMax);
+  // isMobile = signal(window.innerWidth <= this.mobileMax);
+  // isTablet = signal(
+  //   window.innerWidth > this.mobileMax && window.innerWidth <= this.tabletMax
+  // );
+  // isDesktop = signal(window.innerWidth > this.tabletMax);
+
+  constructor() {
+    fromEvent(window, 'resize')
+      .pipe(debounceTime(100), startWith(null))
+      .subscribe(() => this.width.set(window.innerWidth));
+  }
+}
