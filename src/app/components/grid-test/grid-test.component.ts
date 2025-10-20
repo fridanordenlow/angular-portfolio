0ccab8329d8c6cdc989a282eb88cf39eddad141a
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-20 15:58:19 +0200

    create test component for grid auto fill layout

---
+++ src/app/components/grid-test/grid-test.component.ts
@@ -0,0 +1,11 @@
+import { Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-test',
+  imports: [],
+  templateUrl: './grid-test.component.html',
+  styleUrl: './grid-test.component.scss',
+})
+export class GridTestComponent implements OnInit {
+  ngOnInit(): void {}
+}
