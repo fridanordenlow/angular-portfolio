68b359c610490d99c4a4573b3c0ca984ed161e7d
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-12-02 15:01:53 +0100

    add hamburger menu to mobile view

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,34 @@
 import { Component, input } from '@angular/core';
-import { NavbarComponent } from '../navbar/navbar.component';
+// import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterLink, RouterModule } from '@angular/router';
+
+interface NavLink {
+  path: string;
+  label: string;
+  exact?: boolean;
+}
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [RouterModule, RouterLink],
+  // imports: [NavbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  // title = 'Frida Nordenlöw Portfolio';
   title = input.required<string>();
+
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  links: NavLink[] = [
+    { path: '/', label: 'Home', exact: true },
+    { path: 'projects', label: 'Projects' },
+    { path: 'cv', label: 'CV' },
+    { path: 'contact', label: 'Contact' },
+  ];
 }
